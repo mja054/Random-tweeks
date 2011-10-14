2d1a075036ce7dcc06b9a8cc5d6549fd44bb1306
junaid <mdjunaid.54@gmail.com>
2011-10-14 12:28:46 +0530

    myufo: Upload a file on GET.

--- myufo/myufo/proxy/server.py
+++ myufo/myufo/proxy/server.py
@@ -1,4 +1,5 @@
 from webob import Request, Response
+import os
 
 def print_callstack():
     try:
@@ -8,6 +9,22 @@ def print_callstack():
     except ValueError:
         print '------------------------'
 
+class Diskfile(object):
+    def __init__(self, path):
+        if not os.path.exists(os.path.join('/tmp', path.strip('/'))):
+            self.error = True
+        else:
+            self.error = False
+            self.fp = file(os.path.join('/tmp', path.strip('/')), 'r+')
+
+    def __iter__(self):
+        while True:
+            chunk = self.fp.read(4096)
+            if chunk:
+                yield chunk
+            else:
+                break
+
 class Application():
     def __init__(self, conf):
         print 'In __init__'
@@ -15,13 +32,21 @@ class Application():
 
     def GET(self, req):
         print 'In GET()'
-        res = Response(request=req)
-        res.status = '200 OK'
-        res.headerlist = [('Content-type', 'text/html')]
-        res.body = 'Hi, this is awesome.\n'
+        resp_headers = {'user-name': 'junaid',
+                        'Content-type': 'text/html'}
+        file = Diskfile(req.path_info)
+        print 'path_info: ', req.path_info
+        if file.error:
+            res = Response(request=req, headers=resp_headers)
+            res.status = '503 File not found'
+        else:
+            res = Response(request=req, headers=resp_headers, app_iter=file)
+            res.status = '200 OK'
+            res.headerlist = [('Content-type', 'application/octet-stream')]
         return res
 
     def __call__(self, env, start_response):
+        print 'env: ', env
         req = Request(env)
         if req.method == 'GET':
             return self.GET(req)(env, start_response)
